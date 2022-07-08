0fcdd50d2890439c68d365fdc8390bf9805ba35f
Warrior of code <85030860+OscarE-Debug@users.noreply.github.com>
2022-07-08 17:32:30 -0500

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,79 @@
+// *Grab dom elements of navbar responsive
+
+let navbarMobile = document.querySelector(".navigation-mobile");
+let buttonOpenNav = document.querySelector(".bi-list");
+
+// *Window event listener window width
+
+window.addEventListener("resize", () => {
+  windowWidth = screen.width;
+  if (windowWidth > 492) {
+    navbarMobile.classList.remove("navigation-mobile");
+  } else {
+    navbarMobile.classList.add("navigation-mobile");
+  }
+});
+
+window.addEventListener("load", () => {
+  windowWidth = screen.width;
+  if (windowWidth > 492) {
+    navbarMobile.classList.remove("navigation-mobile");
+  } else {
+    navbarMobile.classList.add("navigation-mobile");
+  }
+});
+
+// *Navbar open
+
+buttonOpenNav.addEventListener("click", () => {
+  navbarMobile.classList.toggle("active");
+});
+
+// *Scrollreveal config
+
+ScrollReveal({
+  delay: 300,
+  distance: "0px",
+  duration: 600,
+  easing: "cubic-bezier(0.5, 0, 0, 1)",
+  interval: 0,
+  opacity: 0,
+  origin: "bottom",
+  rotate: {
+    x: 0,
+    y: 0,
+    z: 0,
+  },
+  scale: 1,
+  cleanup: false,
+  container: document.documentElement,
+  desktop: true,
+  mobile: true,
+  reset: true,
+  useDelay: "always",
+  viewFactor: 0.0,
+  viewOffset: {
+    top: 0,
+    right: 0,
+    bottom: 0,
+    left: 0,
+  },
+  afterReset: function (el) {},
+  afterReveal: function (el) {},
+  beforeReset: function (el) {},
+  beforeReveal: function (el) {},
+});
+
+ScrollReveal().reveal(".item-reveal");
+ScrollReveal().reveal(".image-reveal");
+
+// *Hero items reveal
+
+ScrollReveal({
+  scale: 0.7,
+}).reveal(".background-items-hero.item-1");
+
+ScrollReveal({
+  delay: 500,
+  scale: 0.7,
+}).reveal(".background-items-hero.item-2");
